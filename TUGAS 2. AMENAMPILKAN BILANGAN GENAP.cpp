5dcc8f42d7db0fc778f36fdc768af52173abe873
ekayuliaa11 <45647196+ekayuliaa11@users.noreply.github.com>
2019-01-03 23:54:09 +0700

    Add files via upload

---
+++ TUGAS 2. AMENAMPILKAN BILANGAN GENAP.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i=1;
+    do{
+        if (i%2==0){
+            printf("bilangan genap %d\n",i);}
+            i++;
+    }
+    while(i<=10);
+    return 0;
+}
